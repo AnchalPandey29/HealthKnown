d5c249943fc87eaade6cb23ea11759440760dc01
AnchalPandey29 <anchal29pandey@gmail.com>
2025-10-29 18:09:59 +0530

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home'
 
 function App() {
  
   return (
-    <>
-     
-       
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
